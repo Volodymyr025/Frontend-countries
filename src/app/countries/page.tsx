cd9f92df42da8135f1923487fe5177ebdf3e7849
Volodymyr025 <Volgankevych@gmail.com>
2024-11-25 21:37:00 +0200

    Create backend

---
+++ src/app/countries/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ListPage from "../components/ui/ListPage";
+
+const Countries = () => {
+  return (
+    <div>
+      <ListPage />
+    </div>
+  );
+};
+
+export default Countries;
